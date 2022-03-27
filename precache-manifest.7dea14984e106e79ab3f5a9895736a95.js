73581380091989402c73d9fe7769f75365e8c0c4
syedmuhammadkashan <syedkashan38@gmail.com>
2022-03-27 21:25:50 +0500

    Updates

---
+++ precache-manifest.7dea14984e106e79ab3f5a9895736a95.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab01d0c57311f0584f6a5b9804944843",
+    "url": "/shop_reactjs/index.html"
+  },
+  {
+    "revision": "944cc22404a3dbd7daa5",
+    "url": "/shop_reactjs/static/css/main.551086f2.chunk.css"
+  },
+  {
+    "revision": "8c08e945ea335e9f95b2",
+    "url": "/shop_reactjs/static/js/2.80ddbfa7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/shop_reactjs/static/js/2.80ddbfa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944cc22404a3dbd7daa5",
+    "url": "/shop_reactjs/static/js/main.e07ee214.chunk.js"
+  },
+  {
+    "revision": "59584c81103e8cb4914e",
+    "url": "/shop_reactjs/static/js/runtime-main.3abba9e0.js"
+  },
+  {
+    "revision": "189b8e48290f723c95543f727c82c023",
+    "url": "/shop_reactjs/static/media/banner-1.189b8e48.webp"
+  },
+  {
+    "revision": "0133f90e80b95b3ef94a3a04dd276821",
+    "url": "/shop_reactjs/static/media/banner-2.0133f90e.webp"
+  },
+  {
+    "revision": "345ff941cdb4e5d81906aa0915226dc1",
+    "url": "/shop_reactjs/static/media/banner-3.345ff941.webp"
+  },
+  {
+    "revision": "7fb03004737c4f8d8fed8b0e9d3858c8",
+    "url": "/shop_reactjs/static/media/cart.7fb03004.webp"
+  },
+  {
+    "revision": "565895f3db4b5afb7025283fc78f3aff",
+    "url": "/shop_reactjs/static/media/category-banner.565895f3.jpg"
+  },
+  {
+    "revision": "846858cdc9fb2f7c8391bd9cfc7a90bc",
+    "url": "/shop_reactjs/static/media/contact-banner.846858cd.jpg"
+  },
+  {
+    "revision": "c2b3aaca609d90b6ddcb05763be3f5bb",
+    "url": "/shop_reactjs/static/media/product-1.c2b3aaca.jpg"
+  },
+  {
+    "revision": "495f8944f844e8b173cd46c598145a37",
+    "url": "/shop_reactjs/static/media/product-2.495f8944.jpg"
+  },
+  {
+    "revision": "8ff0d0c4c147903ab3f1905be5e252ab",
+    "url": "/shop_reactjs/static/media/product-3.8ff0d0c4.jpg"
+  },
+  {
+    "revision": "7b9f6800e50203c301642a70b4cfa257",
+    "url": "/shop_reactjs/static/media/product-4.7b9f6800.jpg"
+  },
+  {
+    "revision": "b2ec9e1019f248437f6f101582f5a2c3",
+    "url": "/shop_reactjs/static/media/product-5.b2ec9e10.jpg"
+  },
+  {
+    "revision": "4ba9a4dab1c2d9faf0f9b393caabcc8b",
+    "url": "/shop_reactjs/static/media/product-6.4ba9a4da.jpg"
+  },
+  {
+    "revision": "7d82be83f8c4f19232e256c005ed6d67",
+    "url": "/shop_reactjs/static/media/product-7.7d82be83.jpg"
+  },
+  {
+    "revision": "170760a1b7f18f67b7fd7b4676513710",
+    "url": "/shop_reactjs/static/media/product-8.170760a1.jpg"
+  }
+]);
